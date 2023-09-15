000f589e1047f125ef7124ce4aa6a38fe205a9be
FlaTrix <flatrix@pop-os.localdomain>
2023-09-16 01:50:48 +0200

    reset

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,53 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import IndexView from "../views/IndexView.vue";
-import LoginView from "../views/LoginView.vue";
-import ScoreCardView from "../views/ScoreCardView.vue";
-import PubView from "../views/PubView.vue";
-import MainLayout from "../layouts/MainLayout.vue";
+import { createRouter, createWebHistory } from "vue-router"
+import HomeView from "../views/HomeView.vue"
+import IndexView from "../views/IndexView.vue"
+import LoginView from "../views/LoginView.vue"
+import ScoreCardView from "../views/ScoreCardView.vue"
+import PubView from "../views/PubView.vue"
+import MainLayout from "../layouts/MainLayout.vue"
+import ResetView from "../views/ResetView.vue"
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/login",
-            name: "login",
-            component: LoginView,
-        },
-        {
-            path: "/",
-            name: "",
-            component: MainLayout,
-            children: [
-                {
-                    path: "/",
-                    name: "index",
-                    component: IndexView,
-                },
-                {
-                    path: "/home",
-                    name: "home",
-                    component: HomeView,
-                },
-                {
-                    path: "/scorecard",
-                    name: "scorecard",
-                    component: ScoreCardView,
-                },
-                {
-                    path: "/pub",
-                    name: "pub",
-                    component: PubView,
-                },
-            ],
-        },
-    ],
-});
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/login",
+			name: "login",
+			component: LoginView,
+		},
+		{
+			path: "/reset",
+			name: "reset",
+			component: ResetView,
+		},
+		{
+			path: "/",
+			name: "",
+			component: MainLayout,
+			children: [
+				{
+					path: "/",
+					name: "index",
+					component: IndexView,
+				},
+				{
+					path: "/home",
+					name: "home",
+					component: HomeView,
+				},
+				{
+					path: "/scorecard",
+					name: "scorecard",
+					component: ScoreCardView,
+				},
+				{
+					path: "/pub",
+					name: "pub",
+					component: PubView,
+				},
+			],
+		},
+	],
+})
 
-export default router;
+export default router
---
+++ src/views/ResetView.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { get, getDatabase, ref, set, onValue } from "firebase/database"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+
+const db = getDatabase()
+
+const setData = async () => {
+	set(ref(db), null)
+		.then(() => {
+			router.push("/home")
+		})
+		.catch((error) => {
+			error.value = true
+		})
+}
+
+setData()
+</script>
+<template>
+	<div></div>
+</template>
