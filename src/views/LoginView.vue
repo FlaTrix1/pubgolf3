790a7ffe3ddefe0b3baeb6308d0802e77347b196
FlaTrix <flatrix@pop-os.localdomain>
2023-09-15 21:31:38 +0200

    removed

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,4 +1,17 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue"
+import { useGlobalStore } from "@/stores/global"
+import { useRouter } from "vue-router"
+
+const router = useRouter()
+
+const username = ref("")
+const onSubmit = () => {
+	const store = useGlobalStore()
+	store.username = username.value
+	router.replace("/home")
+}
+</script>
 
 <template>
 	<div>
@@ -8,6 +21,8 @@
 			>Set your name</label
 		>
 		<input
+			v-model="username"
+			@keydown.enter="onSubmit"
 			type="text"
 			id="first_name"
 			class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
@@ -15,6 +30,7 @@
 			required
 		/>
 		<button
+			@click="onSubmit"
 			class="bg-blue-500 hover:bg-blue-600 text-white font-medium w-full text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none px-4 py-2.5 mt-4"
 		>
 			Submit
