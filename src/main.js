d8498f071aea3637275fe2e4e533d20d173db2ae
FlaTrix <flatrix@pop-os.localdomain>
2023-09-15 20:38:49 +0200

    init firebase

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
-import { createApp } from "vue";
-import { createPinia } from "pinia";
+import { createApp } from "vue"
+import { createPinia } from "pinia"
 
-import App from "./App.vue";
-import router from "./router";
+import App from "./App.vue"
+import router from "./router"
 
-import "./assets/main.css";
+import "./assets/main.css"
 
-const app = createApp(App);
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-app.use(createPinia());
-app.use(router);
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyAZY02zLOGz07n9nKOJUrtS_579_jfuGjs",
+	authDomain: "pubgolf3.firebaseapp.com",
+	projectId: "pubgolf3",
+	storageBucket: "pubgolf3.appspot.com",
+	messagingSenderId: "357117057229",
+	appId: "1:357117057229:web:c9c062a549ad3549caf184",
+}
 
-app.mount("#app");
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig)
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount("#app")
